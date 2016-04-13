06bedfcd2e5545abf709e50a87364877f42c07a9
ryanlandvater <ryan.landvater@med.uvm.edu>
2016-04-12 22:26:47 -0400

    Revert "Logistical Cleanup #2"
    This reverts commit 73fd03760566b7ad708ae69e514e2254e3b63894.

---
+++ SherlockFilter.m
@@ -0,0 +1,43 @@
+//
+//  SherlockFilter.m
+//  Sherlock
+//
+//  Copyright (c) 2016 Ryan. All rights reserved.
+//
+
+#import "SherlockFilter.h"
+
+@implementation SherlockFilter
+
+- (void) initPlugin
+{
+}
+- (SherlockController*) SherlockControllerObj {
+    if (!_SherlockControllerObj) {
+        _SherlockControllerObj = [[SherlockController alloc] initWithString:@"TestString"];
+    }
+    return _SherlockControllerObj;
+}
+
+- (long) filterImage:(NSString*) menuName
+{	/*ViewerController	*new2DViewer;
+	
+	// In this plugin, we will simply duplicate the current 2D window!
+	
+	new2DViewer = [self duplicateCurrent2DViewerWindow];
+	
+	if( new2DViewer) return 0; // No Errors
+	else return -1;*/
+    NSString* textToDisplay = self.SherlockControllerObj.string;
+    
+    NSAlert *myAlert=[NSAlert alertWithMessageText:textToDisplay
+                                     defaultButton:@"Hello"
+                                   alternateButton:nil
+                                       otherButton:nil
+                         informativeTextWithFormat:@":-)"];
+    [myAlert runModal];
+    return 0;
+ 
+}
+
+@end
